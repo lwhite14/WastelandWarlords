0dfaf3f5fa7fe1fe83bdcc845f01371e58134758
Luke White <luke_white@yahoo.com>
2022-10-09 20:43:35 +0100

    Restructured map classes

--- Assets/Scripts/Maps/CurrentMap.cs
+++ Assets/Scripts/Maps/CurrentMap.cs
@@ -10,10 +10,13 @@ public class CurrentMap : MonoBehaviour
 
     Map[] allMaps =
     {
-        new EnglishChannelMap(),
-        new WalesMap()
+        new Map(@"Assets\Maps\EnglishChannelMap.yaml"),
+        new Map(@"Assets\Maps\WalesMap.yaml")
     };
 
+    [Tooltip(   "1: EnglishChannel\n" +
+                "2: Wales"),
+     Range(0, 1)]
     public int mapUsed = 0;
 
     private void Awake()
